8e47ceed22e1742489716a3d9a98861161a3d039
ArdentApiarist <charles.william.reid@gmail.com>
2019-09-01 10:53:31 +1200

    New changes to helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,2 +1,5 @@
 #Now we make some changes
-print "Hello World and Marker, Your looking great today nice hat"
+print "Hello World and Marker, Your looking great today, I like your shoes"
+
+#Now we make more changes to check out the merge feature.
+
